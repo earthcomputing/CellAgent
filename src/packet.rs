aaa431fc833e1f04d34c3dbea671b6eb056a9f7c
alanhkarp <alanhkarp@gmail.com>
2017-03-13 21:36:47 -0700

    Minor updates to Packet
    I forgot to include the direction, rootward or leafward, when printing
    Packet.  I also made some minor changes to stringify and Clone.

--- src/packet.rs
+++ src/packet.rs
@@ -3,20 +3,29 @@ use std::fmt;
 pub struct Packet {
 	index: u32,
 	is_rootcast: bool,
-	packet: [char; 32],
+	payload: [char; 64],
 }
 impl Packet {
-	fn stringify(&self) -> String {
-		let mut s = format!("Table Index {}: {:?}", self.index, self.packet);
-		for c in self.packet.iter() {
-			s = s + &c.to_string();
+	pub fn new(index: u32, is_rootcast: bool, payload: [char; 64] ) -> Packet {
+		Packet { index: index, is_rootcast: is_rootcast, payload: payload }
+	}
+	pub fn stringify(&self) -> String {
+		let mut s = format!("Table Index {}: ", self.index);
+		if self.is_rootcast { s = s + "Rootward"; }
+		else                { s = s + "Leafward"; }
+		s = s + ": Payload = ";
+		for c in self.payload.iter() {
+			s.push(*c);
 		}
 		s
 	}
 }
 impl Clone for Packet {
-	fn clone(&self) -> Packet { Packet { index: self.index, packet: self.packet, is_rootcast: false } }
+	fn clone(&self) -> Packet { *self }
 }
 impl fmt::Debug for Packet { 
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", self.stringify()) }
+}
+impl fmt::Display for Packet { 
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", self.stringify()) }
 }
