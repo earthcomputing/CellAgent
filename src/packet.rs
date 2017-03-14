a218855934e31a0ba0acc6e277260170a5ff9011
alanhkarp <alanhkarp@gmail.com>
2017-03-14 14:15:51 -0700

    Stupid me
    I forgot to include message.rs and packet.rs in the last commit.

--- src/packet.rs
+++ src/packet.rs
@@ -1,18 +1,21 @@
 use std::fmt;
 #[derive(Copy)]
-pub struct Packet {
+pub struct Packet64 {
 	index: u32,
+	sending_port: u8,
+	receiving_port: u8,
 	is_rootcast: bool,
-	payload: [char; 64],
+	payload: [char; 56],
 }
-impl Packet {
-	pub fn new(index: u32, is_rootcast: bool, payload: [char; 64] ) -> Packet {
-		Packet { index: index, is_rootcast: is_rootcast, payload: payload }
+impl Packet64 {
+	pub fn new(index: u32, is_rootcast: bool, payload: [char; 56] ) -> Packet64 {
+		Packet64 { index: index, sending_port: 0, receiving_port: 0, is_rootcast: is_rootcast, payload: payload }
 	}
 	pub fn stringify(&self) -> String {
 		let mut s = format!("Table Index {}: ", self.index);
 		if self.is_rootcast { s = s + "Rootward"; }
 		else                { s = s + "Leafward"; }
+		s = s + &format!(", Sending port {}", self.sending_port);
 		s = s + ": Payload = ";
 		for c in self.payload.iter() {
 			s.push(*c);
@@ -20,12 +23,12 @@ impl Packet {
 		s
 	}
 }
-impl Clone for Packet {
-	fn clone(&self) -> Packet { *self }
+impl Clone for Packet64 {
+	fn clone(&self) -> Packet64 { *self }
 }
-impl fmt::Debug for Packet { 
+impl fmt::Debug for Packet64 { 
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", self.stringify()) }
 }
-impl fmt::Display for Packet { 
+impl fmt::Display for Packet64 { 
 	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "{}", self.stringify()) }
 }
