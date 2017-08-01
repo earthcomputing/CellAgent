b3d166418f57409e594fcc05737cac4d0240cb4b
alanhkarp <alanhkarp@gmail.com>
2017-08-01 15:20:33 -0700

    Use local variables in GVM equation
    I was only using constant expressions for the GVM equation.  This
    untested version allows me to include the evaluating cell’s hops from
    the parent.

--- src/gvm_equation.rs
+++ src/gvm_equation.rs
@@ -12,17 +12,16 @@ impl GvmEquation {
 	pub fn new(s: &str, variables: GvmVariables) -> GvmEquation { 
 		GvmEquation { equation: s.to_string(), variables: variables }
 	}
-	pub fn evaluate(&self, params: HashMap<&str,&str>) -> Result<bool> {
+	pub fn evaluate(&self, params: HashMap<String,String>) -> Result<bool> {
 		let mut expr = Expr::new(self.equation.clone());
 		for (variable, value) in params {
-			let test: bool = serde_json::from_str(value).chain_err(|| ErrorKind::GvmEquationError)?;
+			let test: bool = serde_json::from_str(&value).chain_err(|| ErrorKind::GvmEquationError)?;
 			expr = expr.clone().value(variable,test);
 		}
-		let result = serde_json::from_str(&expr.exec().
-			chain_err(|| ErrorKind::GvmEquationError)?.
-			to_string()).chain_err(|| ErrorKind::GvmEquationError)?;
-		Ok(result)
+		let result = expr.exec().chain_err(|| ErrorKind::GvmEquationError)?;
+		Ok(result == to_value(true))
 	}
+	pub fn get_variables(&self) -> &GvmVariables { &self.variables }
 }
 #[derive(Debug, Clone, Hash, Serialize, Deserialize)]
 pub struct GvmVariables {
@@ -36,6 +35,7 @@ impl GvmVariables {
 	pub fn empty() -> GvmVariables {
 		GvmVariables { variables: Vec::new() }
 	}
+	pub fn iter(&self) -> ::std::slice::Iter<String> { self.variables.iter() } 
 }
 error_chain! {
 	foreign_links {
