e9be836b7c533b16ba5594acf60b28d9db7b588f
Bill Jackson <bjackson@earthcomputing.io>
2019-05-06 16:32:22 -0700

    inline ENTL_skb_queue methods

--- src/entl_skb_queue.h
+++ src/entl_skb_queue.h
@@ -1,7 +1,6 @@
 #ifndef _ENTL_SKB_QUEUE_H_
 #define _ENTL_SKB_QUEUE_H_
 
-// ref: xx
 #define ENTL_DEFAULT_TXD   256
 typedef struct ENTL_skb_queue {
     uint16_t size;
@@ -11,28 +10,18 @@ typedef struct ENTL_skb_queue {
     struct sk_buff *data[ENTL_DEFAULT_TXD];
 } ENTL_skb_queue_t ;
 
-// FIXME: add these as inline methods
-static void init_ENTL_skb_queue(ENTL_skb_queue_t *q);
-static int ENTL_skb_queue_full(ENTL_skb_queue_t *q);
-static int ENTL_skb_queue_has_data(ENTL_skb_queue_t *q);
-static int ENTL_skb_queue_unused(ENTL_skb_queue_t *q);
-static struct sk_buff *front_ENTL_skb_queue(ENTL_skb_queue_t *q);
-static struct sk_buff *pop_front_ENTL_skb_queue(ENTL_skb_queue_t *q);
-static int push_back_ENTL_skb_queue(ENTL_skb_queue_t *q, struct sk_buff *dt);
-
-#if 0
-static void init_ENTL_skb_queue(ENTL_skb_queue_t *q) {
+static inline void ENTL_skb_queue_init(ENTL_skb_queue_t *q) {
     q->size = E1000_DEFAULT_TXD;
     q->count = 0;
     q->head = q->tail = 0;
 }
 
-static int ENTL_skb_queue_full(ENTL_skb_queue_t *q) { return (q->size == q->count) ? 1 : 0 }
-static int ENTL_skb_queue_has_data(ENTL_skb_queue_t *q) { return q->count; }
-static int ENTL_skb_queue_unused(ENTL_skb_queue_t *q) { return q->size - q->count - 1; }
+static inline int ENTL_skb_queue_has_data(ENTL_skb_queue_t *q) { return q->count; }
+static inline int ENTL_skb_queue_unused(ENTL_skb_queue_t *q) { return q->size - q->count - 1; }
+static inline int ENTL_skb_queue_full(ENTL_skb_queue_t *q) { return (q->size == q->count) ? 1 : 0; }
 
-static struct sk_buff *front_ENTL_skb_queue(ENTL_skb_queue_t *q) { return (q->count == 0) ? NULL : q->data[q->head]; }
-static struct sk_buff *pop_front_ENTL_skb_queue(ENTL_skb_queue_t *q) {
+static inline struct sk_buff *ENTL_skb_queue_front(ENTL_skb_queue_t *q) { return (q->count == 0) ? NULL : q->data[q->head]; }
+static inline struct sk_buff *ENTL_skb_queue_front_pop(ENTL_skb_queue_t *q) {
     if (q->count == 0) return NULL;
     struct sk_buff *dt = q->data[q->head];
     q->head = (q->head + 1) % q->size;
@@ -40,13 +29,12 @@ static struct sk_buff *pop_front_ENTL_skb_queue(ENTL_skb_queue_t *q) {
     return dt;
 }
 
-static int push_back_ENTL_skb_queue(ENTL_skb_queue_t *q, struct sk_buff *dt) {
+static inline int ENTL_skb_queue_back_push(ENTL_skb_queue_t *q, struct sk_buff *dt) {
     if (q->size == q->count) return -1;
     q->data[q->tail] = dt;
     q->tail = (q->tail+1) % q->size;
     q->count++;
     return q->size - q->count;
 }
-#endif
 
 #endif
