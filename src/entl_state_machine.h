d6a419a2de46ebb51a70199737c2f4fb89cd1944
Bill Jackson <bjackson@earthcomputing.io>
2019-05-06 13:43:06 -0700

    entl_state_machine separately compiled (extern)

--- src/entl_state_machine.h
+++ src/entl_state_machine.h
@@ -23,13 +23,13 @@
 typedef struct entl_state_machine {
 } entl_state_machine_t;
 
-static int entl_next_send(entl_state_machine_t *mcn, uint16_t *u_addr, uint32_t *l_addr); // ENTL_ACTION
-static int entl_received(entl_state_machine_t *mcn, uint16_t u_saddr, uint32_t l_saddr, uint16_t u_daddr, uint32_t l_daddr); // ENTL_ACTION
-static void entl_state_error(entl_state_machine_t *mcn, uint32_t error_flag); // enter error state
+int entl_next_send(entl_state_machine_t *mcn, uint16_t *u_addr, uint32_t *l_addr); // ENTL_ACTION
+int entl_received(entl_state_machine_t *mcn, uint16_t u_saddr, uint32_t l_saddr, uint16_t u_daddr, uint32_t l_daddr); // ENTL_ACTION
+void entl_state_error(entl_state_machine_t *mcn, uint32_t error_flag); // enter error state
 
 #include "entl_user_api.h"
 
-static void entl_new_AIT_message(entl_state_machine_t *mcn, struct entt_ioctl_ait_data *data);
-struct entt_ioctl_ait_data *entl_next_AIT_message(entl_state_machine_t *mcn);
+void entl_new_AIT_message(entl_state_machine_t *mcn, struct entt_ioctl_ait_data *data);
+entt_ioctl_ait_data *entl_next_AIT_message(entl_state_machine_t *mcn);
 
 #endif
