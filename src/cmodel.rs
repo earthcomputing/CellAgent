864a317ca89214c3194e097436dc83d2a4db6b41
alanhkarp <alanhkarp@gmail.com>
2019-05-21 17:00:31 -0700

    Got rid of byte arrays in trace records
    cellagent.rs, cmodel.rs, service.rs, and vm.rs were still producing
    byte arrays in the trace records.  I got rid of most of them, but
    there’s nothing I can do about places where all I have is a packet.  I
    could try to deserialize, but that might fail if the message does not
    fit in one packet.

--- src/cmodel.rs
+++ src/cmodel.rs
@@ -7,7 +7,7 @@ use failure::{Error, ResultExt};
 use crate::config::{CENTRAL_TREE, CONTINUE_ON_ERROR, DEBUG_OPTIONS,
                     TRACE_OPTIONS, PortNo};
 use crate::dal::{add_to_trace, fork_trace_header, update_trace_header};
-use crate::ec_message::MsgType;
+use crate::ec_message::{Message, MsgType};
 use crate::ec_message_formats::{CaToCmBytes, CmToCa, CmFromCa, CmToPe, CmFromPe, PeToCmPacket,
                                 CmToPePacket, CmToCaBytes};
 use crate::name::{Name, CellID};
@@ -266,8 +266,9 @@ packets: Vec<Packet>,
             let bytes = Packetizer::unpacketize(&packets).context(CmodelError::Chain { func_name: _f, comment: S("") })?;
             {
                 if TRACE_OPTIONS.all || TRACE_OPTIONS.cm {
+                    let msg: Box<dyn Message> = serde_json::from_str(&bytes.to_string()?)?;
                     let trace_params = &TraceHeaderParams { module: file!(), line_no: line!(), function: _f, format: "cm_to_ca_bytes" };
-                    let trace = json!({ "cell_id": &self.cell_id, "bytes": &bytes.to_string()? });
+                    let trace = json!({ "cell_id": &self.cell_id, "msg": msg.to_string() });
                     let _ = add_to_trace(TraceType::Debug, trace_params, &trace, _f); // sender side, dup
                     if DEBUG_OPTIONS.all || DEBUG_OPTIONS.cm_to_ca {
                         let packet_count = packets[0].get_count();
