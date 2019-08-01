6f5001f1c54894c1ea0dfb5648713d9b5020935c
alanhkarp <alanhkarp@gmail.com>
2019-08-01 10:17:18 -0700

    Moved global declarations from binary roots to lib.rs, with conditional compilation based on binary.
    I had no idea how to set up a Rust project when I started working on
    the code.  In particular, I didn’t know what lib.rs is for.  As a
    result, I put all my “extern crate” and “pub mod” statements in the
    file containing main().  That’s fine, until you get multiple binaries,
    that is.  Since several files contain main(), they each need to be
    changed when the extern crates or modules change.
    
    I have now put those global declarations in lib.rs and taken them out
    of the individual binaries.  Note that a crate that needs an extern
    declaration, those with #[macro_use], needs to appear in the file of
    any main() that uses that macro.
    
    Instead of “use crate::foo” for local modules, I had to “use
    ec_fabrix::foo” in each binary root.

--- src/external_echoDemo.rs
+++ src/external_echoDemo.rs
@@ -1,55 +1,17 @@
-#![deny(unused_must_use)]
-//#![warn(rust_2018_idioms)]
-#![recursion_limit="1024"]
-#![allow(dead_code)]
-#![allow(unused_variables)]
-#![allow(unused_imports)]
 #[macro_use] extern crate failure;
-#[macro_use] extern crate serde_derive;
-#[macro_use] extern crate serde_json;
-
-pub mod app_message;
-pub mod app_message_formats;
-pub mod blueprint;
-pub mod cellagent;
-pub mod config;
-pub mod container;
-pub mod dal;
-pub mod dumpstack;
-pub mod ec_message;
-pub mod ec_message_formats;
-pub mod errors;
-pub mod gvm_equation;
-pub mod name;
-pub mod noc;
-pub mod packet;
-pub mod packet_engine;
-pub mod port;
-pub mod port_tree;
-pub mod routing_table;
-pub mod routing_table_entry;
-pub mod service;
-pub mod tenant;
-pub mod traph;
-pub mod traph_element;
-pub mod tree;
-pub mod uptree_spec;
-pub mod utility;
-pub mod uuid_ec;
-pub mod vm;
 
 use std::{io::{stdin, stdout, Read, Write},
           collections::{HashMap, HashSet},
           fs::{File, OpenOptions},
           sync::mpsc::channel};
 
-use crate::app_message_formats::{ApplicationFromNoc, ApplicationToNoc, NocFromApplication, NocToApplication};
-use crate::blueprint::{Blueprint};
-use crate::config::{CONFIG, CellQty, PortQty};
-use crate::gvm_equation::{GvmEqn};
-use crate::noc::Noc;
-use crate::uptree_spec::{AllowedTree, ContainerSpec, Manifest, UpTreeSpec, VmSpec};
-use crate::utility::{CellConfig, CellNo, PortNo, S, TraceHeader, is2e, _print_vec};
+use ec_fabrix::app_message_formats::{ApplicationFromNoc, ApplicationToNoc, NocFromApplication, NocToApplication};
+use ec_fabrix::blueprint::{Blueprint};
+use ec_fabrix::config::{CONFIG, CellQty, PortQty};
+use ec_fabrix::gvm_equation::{GvmEqn};
+use ec_fabrix::noc::Noc;
+use ec_fabrix::uptree_spec::{AllowedTree, ContainerSpec, Manifest, UpTreeSpec, VmSpec};
+use ec_fabrix::utility::{CellConfig, CellNo, PortNo, S, is2e, _print_vec};
 
 fn main() -> Result<(), Error> {
     let _f = "main";
