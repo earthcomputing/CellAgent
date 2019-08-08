245759a7e9c2b872fb33b76ab42a2eadc9cb4252
alanhkarp <alanhkarp@gmail.com>
2019-08-08 10:43:01 -0700

    Use ArrayString for name.rs
    It’s really annoying having to clone IDs, so I made the name field a
    fixed length array.  That change required conversions between the char
    array and strings for printout and custom serialize/deserialize methods
    to work with the trace record tools.
    
    The arrayvec crate has an ArrayString struct that takes care of that
    for me.  The result is a negative code day and, probably, better
    performance.

--- src/name.rs
+++ src/name.rs
@@ -1,39 +1,21 @@
 use std::{fmt, marker::Sized};
 use std::string::String;
 
-use serde::{Deserialize, Deserializer, Serializer};
+use arrayvec::ArrayString;
 
 use crate::config::{MAX_CHARS, SEPARATOR};
 use crate::utility::{PortNo, PortNumber, S};
 use crate::uuid_ec::Uuid;
 
-const DEFAULT_CHAR: char = '\n';
-type NameType = [char; MAX_CHARS];
+type NameType = ArrayString<[u8; MAX_CHARS]>;
 
-fn name_string<S: Serializer>(name: &NameType, s: S) -> Result<S::Ok, S::Error> {
-    s.serialize_str(&str_from_chars(*name))
-}
-fn string_name<'de, D: Deserializer<'de>>(deserializer: D) -> Result<NameType, D::Error>
-{
-    let s: &str = Deserialize::deserialize(deserializer)?;
-    Ok(str_to_chars(s))
-}
 fn str_to_chars(string: &str) -> NameType {
     let _f = "str_to_chars";
-    if string.len() > MAX_CHARS {
-        // Should never reach here because check is done when name is created
-        panic!(format!("String |{}| is longer than {} characters {}", string, MAX_CHARS, string.len()))
-    }
-    let mut char_slice = [DEFAULT_CHAR; MAX_CHARS];
-    string.char_indices()
-        .for_each(|(i, c)| char_slice[i] = c);
-    char_slice
+    ArrayString::from(string).expect(&format!("String |{}| is longer than {} characters {}", string, MAX_CHARS, string.len()))
 }
 fn str_from_chars(chars: NameType) -> String {
     let _f = "str_from_chars";
-    chars.iter()
-        .take_while(|&c| *c != DEFAULT_CHAR )
-        .fold(S(""), |mut acc, &c| { acc.push(c); acc } )
+    chars.as_str().to_owned()
 }
 
 pub trait Name: Sized {
@@ -59,8 +41,6 @@ pub trait Name: Sized {
 }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct CellID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -80,8 +60,6 @@ impl Name for CellID {
 impl fmt::Display for CellID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct PortID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -99,8 +77,6 @@ impl Name for PortID {
 impl fmt::Display for PortID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct TreeID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -142,8 +118,6 @@ impl fmt::Display for TreeID {
 }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct PortTreeID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -173,8 +147,6 @@ impl fmt::Display for PortTreeID {
 }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct UptreeID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -195,8 +167,6 @@ impl Name for UptreeID {
 impl fmt::Display for UptreeID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct TenantID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -219,8 +189,6 @@ impl Name for TenantID {
 impl fmt::Display for TenantID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize)]
 pub struct LinkID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -238,8 +206,6 @@ impl Name for LinkID {
 impl fmt::Display for LinkID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct VmID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -257,8 +223,6 @@ impl Name for VmID {
 impl fmt::Display for VmID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct SenderID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
@@ -276,8 +240,6 @@ impl Name for SenderID {
 impl fmt::Display for SenderID { fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result { str_from_chars(self.name).fmt(f) } }
 #[derive(Debug, Copy, Clone, PartialEq, Eq, Hash, Serialize, Deserialize)]
 pub struct ContainerID {
-    #[serde(serialize_with = "name_string")]
-    #[serde(deserialize_with = "string_name")]
     name: NameType,
     uuid: Uuid
 }
