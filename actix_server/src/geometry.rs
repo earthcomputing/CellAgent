71c3108e3376b05769dadae6389712d1ddaf1563
alanhkarp <alanhkarp@gmail.com>
2019-08-29 14:05:31 -0700

    Geometry shows cell id instead of cell number
    Previously, getting geometry return a cell number and the location of
    that cell.  That required correlating cell name with cell number for
    later steps.  I just noticed that the geometry trace records also
    include the cell id, so that’s what I’m using now.

--- actix_server/src/geometry.rs
+++ actix_server/src/geometry.rs
@@ -1,10 +1,10 @@
 use std::{cmp::max,
           collections::HashMap,
           fmt, fmt::Write,
-          sync::{Arc, Mutex}
+          sync::{Mutex}
 };
 
-use actix_web::{web, Error, HttpRequest, HttpResponse, Responder};
+use actix_web::{web, Error, HttpResponse, Responder};
 use serde::{Deserialize, Serialize};
 use serde_json::Value;
 
@@ -12,46 +12,35 @@ type Size = usize;
 
 pub fn cell_geometry(path: &str, state: web::Data<AppGeometry>, record: web::Json<Value>)
                  -> Result<impl Responder, Error> {
+    let trace_body = record.get("body").expect("HelloMsg: bad trace record");
+    let body: Body = serde_json::from_value(trace_body.clone())?;
+    let name = body.cell_id.name;
+    let location = body.location;
     let app_geometry = state.get_ref();
-    if let Some(body) = record.get("body") {
-        let geo = serde_json::from_value::<GeoStruct>(body.clone())?;
-        app_geometry.geometry.lock().unwrap().add(Id::new(geo.cell_number),
-                                                  Location::new(geo.location));
-    }
+    app_geometry.geometry.lock().unwrap().add(CellID { name },
+                                              Location { row: location[0], col: location[1] });
     Ok(HttpResponse::Ok().body(format!("{} adding {}\n{:?}\n", path, record, app_geometry)))
 }
-#[derive(Debug, Copy, Clone, Serialize, Deserialize)]
-struct GeoStruct {
-    cell_number: usize,
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Body {
+    cell_id: CellID,
     location: [usize; 2]
+}
+#[derive(Debug, Clone, Hash, Eq, PartialEq, Serialize, Deserialize)]
+pub struct CellID {
+    name: String
 }
 #[derive(Debug, Default, Serialize)]
 pub struct AppGeometry {
     geometry: Mutex<Geometry>
-}
-
-#[derive(Debug, Copy, Clone, Default, Eq, PartialEq, Serialize, Deserialize)]
-pub struct Id(Size);
-impl Id {
-    pub fn new(id: Size) -> Id { Id(id) }
-}
-impl fmt::Display for Id {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Id {}", self.0)
-    }
 }
 #[derive(Debug, Copy, Clone, Default, Eq, PartialEq, Serialize)]
 pub struct Location { row: Size, col: Size }
 impl Location {
     pub fn new(rowcol: [Size; 2]) -> Location { Location { row: rowcol[0], col: rowcol[1] } }
 }
-impl fmt::Display for Location {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "Row {} Col {}", self.row, self.col)
-    }
-}
 
-type RowCol = HashMap<Size, Location>;
+type RowCol = HashMap<String, Location>;
 
 #[derive(Debug, Clone, Eq, PartialEq, Default, Serialize)]
 pub struct Geometry {
@@ -60,23 +49,9 @@ pub struct Geometry {
     rowcol: RowCol
 }
 impl Geometry {
-    pub fn add(&mut self, id: Id, rowcol: Location) { 
+    pub fn add(&mut self, cell_id: CellID, rowcol: Location) {
         self.maxrow = max(self.maxrow, rowcol.row);
         self.maxcol = max(self.maxrow, rowcol.col);
-        self.rowcol.insert(id.0, rowcol); 
+        self.rowcol.insert(cell_id.name, rowcol);
     }
-}
-impl fmt::Display for Geometry {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut s = format!("\nCell layout");
-        if self.rowcol.is_empty() {
-            write!(f, "{}", "No cells")
-        } else {
-            write!(s, "\n\n    Cell  Row  Col")?;
-            for (id, rowcol) in &self.rowcol {
-                write!(s, "\n    {:4} {:4} {:4}", id, rowcol.row, rowcol.col)?;
-            }
-            write!(f, "{}\n", s)
-        }
-     }
 }
