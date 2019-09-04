ff08a798ede08c69d7908de861a6170b38b115f4
alanhkarp <alanhkarp@gmail.com>
2019-09-04 14:34:15 -0700

    Use client-side JavaScript to change the DOM
    The server provides a web page that client-side Javascript can modify.

--- actix_server/src/main.rs
+++ actix_server/src/main.rs
@@ -1,16 +1,27 @@
-use std::{env};
+use std::{env,
+          fs,
+};
+use actix_files;
 
-use actix_web::{web, App, HttpServer, Responder};
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
 
 use ec_trace_analyzer::{geometry, hello, index};
 
 fn main() {
     let server_url = env::var("SERVER_URL").expect("Environment variable SERVER_URL not found");
     println!("Server at {}", server_url);
+    let html_file_name = env::var("HTML").expect("Environment variable HTML not found");
+    println!("html_file_name {}", html_file_name);
+    let index_data = web::Data::new(html_file_name);
     let geo_data = geometry::data();
     let hello_data = hello::data();
+    let css = fs::read_to_string("./html/visualizer.css").expect("Cannot read CSS file");
     HttpServer::new(move || {
         App::new()
+            .route("/visualizer.css", web::get().to(get_css))
+            .route("/visualize.js", web::get().to(get_visualizer))
+            
+            .register_data(index_data.clone())
             .route("/",web::get().to(index::index))
             
             .register_data(geo_data.clone())
@@ -26,4 +37,16 @@ fn main() {
         .unwrap()
         .run()
         .unwrap();
+}
+fn get_css() -> impl Responder {
+    let css = fs::read_to_string("./html/visualizer.css").expect("Cannot read CSS file");
+    HttpResponse::Ok()
+        .content_type("text/css")
+        .body(css)
+}
+fn get_visualizer() -> impl Responder {
+    let viz = fs::read_to_string("./html/visualize.js").expect("Cannot read JavaScript file");
+    HttpResponse::Ok()
+        .content_type("application/javascript")
+        .body(viz)
 }
