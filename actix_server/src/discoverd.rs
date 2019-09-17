693611a781a301928cf0dd12c73abfff95940129
alanhkarp <alanhkarp@gmail.com>
2019-09-17 13:50:01 -0700

    Can show all trees without reloading page
    Click on a node and see all the links in that tree.  Click on another,
    and see the links in the other tree.

--- actix_server/src/discoverd.rs
+++ actix_server/src/discoverd.rs
@@ -14,7 +14,6 @@ use crate::hello::{AppCell, AppCells, LinkType, Neighbor};
 type Size = usize;
 
 fn show_black_tree(appcells: web::Data<AppCells>) -> Result<HttpResponse, actix_web::Error> {
-    println!("Show black tree");
     let appcells = appcells.get_ref();
     let string = serde_json::to_string(appcells)?;
     Ok(HttpResponse::Ok().body(string))
