89a4284df64cba26b23d4fca394ecefbef392abf
alanhkarp <alanhkarp@gmail.com>
2019-09-17 16:32:03 -0700

    Fixed cfg error in build.rs

--- cellagent/build.rs
+++ cellagent/build.rs
@@ -4,6 +4,8 @@ use std::env::var;
 fn main() {
     #[cfg(feature = "cell")]
     let cell_agent_dir = var("CELL_AGENT_DIR").expect("Must set CELL_AGENT_DIR environment variable");
+    #[cfg(feature = "cell")]
     println!(r"cargo:rustc-link-search={}/ecnl", cell_agent_dir);
+    #[cfg(feature = "cell")]
     println!(r"cargo:rustc-link-search={}/../../bjackson-ecnl/lib", cell_agent_dir);
 }
