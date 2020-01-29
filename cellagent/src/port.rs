a3434c8730803107a736690b84d8750bfeb1d9f0
Vida Ferdowsi <vida.ferdowsi@gmail.com>
2020-01-28 16:34:20 -0800

    A prior fix that somehow didn't get included.

--- cellagent/src/port.rs
+++ cellagent/src/port.rs
@@ -328,31 +328,31 @@ impl Port {
                     let _ = add_to_trace(TraceType::Trace, trace_params, &trace, _f);
                 }
             }
+	    let ait_state = packet.get_ait_state();
+            match ait_state {
+                AitState::AitD |
+                AitState::Tick |
+                AitState::Tock |
+                AitState::Tack |
+                AitState::Teck => return Err(PortError::Ait { func_name: _f, ait_state }.into()), // Not allowed here
+                
+                AitState::Ait => {
+                    packet.next_ait_state()?;
+                },
+                AitState::Entl | // Only needed for simulator, should be handled by port
+                AitState::Normal => ()
+            }
+            {
+                if CONFIG.trace_options.all | CONFIG.trace_options.port {
+                    let ait_state = packet.get_ait_state();
+                    let trace_params = &TraceHeaderParams { module: file!(), line_no: line!(), function: _f, format: "port_to_link" };
+                    let trace = json!({ "id": self.get_id().get_name(), "ait_state": ait_state, "packet": packet });
+                    let _ = add_to_trace(TraceType::Trace, trace_params, &trace, _f);
+                }
+            }
             #[cfg(feature = "simulator")]
             {
-                let ait_state = packet.get_ait_state();
-                match ait_state {
-                    AitState::AitD |
-                    AitState::Tick |
-                    AitState::Tock |
-                    AitState::Tack |
-                    AitState::Teck => return Err(PortError::Ait { func_name: _f, ait_state }.into()), // Not allowed here
-                
-                    AitState::Ait => {
-                        packet.next_ait_state()?;
-                    },
-                    AitState::Entl | // Only needed for simulator, should be handled by port
-                    AitState::Normal => ()
-                }
-                {
-                    if CONFIG.trace_options.all | CONFIG.trace_options.port {
-                        let ait_state = packet.get_ait_state();
-                        let trace_params = &TraceHeaderParams { module: file!(), line_no: line!(), function: _f, format: "port_to_link" };
-                        let trace = json!({ "id": self.get_id().get_name(), "ait_state": ait_state, "packet": packet });
-                        let _ = add_to_trace(TraceType::Trace, trace_params, &trace, _f);
-                    }
-                }
-                port_to_link_or_ecnl_port.clone().left().expect("ecnl port in simulator").send(packet)?;
+		port_to_link_or_ecnl_port.clone().left().expect("ecnl port in simulator").send(packet)?;
             }
             #[cfg(feature = "cell")]
             {
