f0627928ab92050a5b029fdcd7e85002cdea9f9e
Steven Ganz <steven.ganz@teamifier.com>
2021-05-05 10:47:44 -0700

    Add license file and put license/copyright banner on source files.  In the process, ensure that all files end with newline.

--- userspace/cellagent/src/dal.rs
+++ userspace/cellagent/src/dal.rs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 // This file contains hacks that represent functions of the DAL.
 // which will be replaced by actual distributed storage algorithms.
 use std::{cell::RefCell,
--- userspace/cellagent/src/snake.rs
+++ userspace/cellagent/src/snake.rs
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 // Snake is a code name used for a strategy to deal with packets that
 // may be lost when a node fails.  As a packet travels from its
 // source to its destination, each cell on the path keeps a copy.
