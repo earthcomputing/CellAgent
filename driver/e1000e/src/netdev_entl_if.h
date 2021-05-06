c2f0e4ed0d3669bb483a25cc1cb9efb249a56813
Steven Ganz <steven.ganz@teamifier.com>
2021-05-05 18:11:54 -0700

    Add license file and put license/copyright banner on source files.  In the process, ensure that all files end with newline.

--- driver/e1000e/src/netdev_entl_if.h
+++ driver/e1000e/src/netdev_entl_if.h
@@ -1,3 +1,7 @@
+/*---------------------------------------------------------------------------------------------
+ *  Copyright © 2016-present Earth Computing Corporation. All rights reserved.
+ *  Licensed under the MIT License. See LICENSE.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
 #ifndef _NETDEV_ENTL_IF_H_
 #define _NETDEV_ENTL_IF_H_
 
